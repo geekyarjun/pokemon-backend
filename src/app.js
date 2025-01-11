999272c7e10b4576c65393d20530d20bbe7c60a4
ArjunTechX360 <arjuns@techx360.co>
2025-01-11 21:54:57 +0400

    feat: added pm2 to manage process

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const helmet = require("helmet");
-const mongoSanitize = require("express-mongo-sanitize");
-const compression = require("compression");
-const cors = require("cors");
-const httpStatus = require("http-status");
-const config = require("./config/config");
-const morgan = require("./config/morgan");
-const routes = require("./routes/v1");
-const { errorConverter, errorHandler } = require("./middlewares/error");
-const ApiError = require("./utils/ApiError");
+const express = require('express');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const compression = require('compression');
+const cors = require('cors');
+const httpStatus = require('http-status');
+const config = require('./config/config');
+const morgan = require('./config/morgan');
+const routes = require('./routes/v1');
+const { errorConverter, errorHandler } = require('./middlewares/error');
+const ApiError = require('./utils/ApiError');
 
 const app = express();
 
-if (config.env !== "test") {
+if (config.env !== 'test') {
   app.use(morgan.successHandler);
   app.use(morgan.errorHandler);
 }
@@ -34,19 +34,14 @@ app.use(compression());
 
 // enable cors
 app.use(cors());
-app.options("*", cors());
-
-// limit repeated failed requests to auth endpoints
-if (config.env === "production") {
-  app.use("/v1/auth", authLimiter);
-}
+app.options('*', cors());
 
 // v1 api routes
-app.use("/api/v1", routes);
+app.use('/api/v1', routes);
 
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
-  next(new ApiError(httpStatus.NOT_FOUND, "Not found"));
+  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
 });
 
 // convert error to ApiError, if needed
